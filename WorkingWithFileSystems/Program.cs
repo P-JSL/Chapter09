5ee3c3d3c84c49a166d01b68bf093da613e25dc6
P-JSL <althsuwpfl@gmail.com>
2023-09-02 11:26:35 +0900

    초기 커밋.

---
+++ WorkingWithFileSystems/Program.cs
@@ -0,0 +1,112 @@
+﻿using static System.Console;
+using static System.IO.Directory;
+using static System.IO.Path;
+using static System.Environment;
+
+OutputfileSystemInfo();
+static void OutputfileSystemInfo()
+{
+    WriteLine("{0, -33} {1}", arg0: "Path.PathSeparator", arg1: Path.PathSeparator);
+    WriteLine("{0, -33} {1}", arg0: "Path.DirectorySeparatorChar", arg1: Path.DirectorySeparatorChar);
+    WriteLine("{0, -33} {1}", arg0: "Directory.GetCurrentDirectory", arg1: Directory.GetCurrentDirectory());
+    WriteLine("{0, -33} {1}", arg0: "Environment.CurrentDirectory", arg1: Environment.CurrentDirectory);
+    WriteLine("{0, -33} {1}", arg0: "Environment.SystemDirectory", arg1: Environment.SystemDirectory);
+    WriteLine("{0, -33} {1}", arg0: "Path.GetTempPath", arg1: Path.GetTempPath());
+    WriteLine("GetFolderPath(SpecialFolder)");
+    WriteLine("{0, -33} {1}", arg0: ".System", arg1: GetFolderPath(SpecialFolder.System));
+    WriteLine("{0, -33} {1}", arg0: ".ApplicationData", arg1: GetFolderPath(SpecialFolder.ApplicationData));
+    WriteLine("{0, -33} {1}", arg0: ".MyDocuments", arg1: GetFolderPath(SpecialFolder.MyDocuments));
+    WriteLine("{0, -33} {1}", arg0: ".Personal", arg1: GetFolderPath(SpecialFolder.Personal));
+
+}
+
+
+// 드라이브
+
+WorkWithDrives();
+static void WorkWithDrives()
+{
+    WriteLine("{0,-30}  |   {1,-10} |   {2,-7}  |   {3,18}  |   {4,18}", "NAME", "TYPE", "FORMAT", "SIZE(BYTES)", "FREE SPACE");
+    foreach(DriveInfo drive in DriveInfo.GetDrives())
+    {
+        if (drive.IsReady)
+        {
+            WriteLine("{0,-30}  |   {1,-10} |   {2,-7}  |   {3,18:N0}  |   {4,18:N0}",
+                drive.Name,drive.DriveType,drive.DriveFormat,drive.TotalSize,drive.AvailableFreeSpace);
+        }
+    }
+}
+
+//디렉토리
+WorkWithDirectories();
+static void WorkWithDirectories()
+{
+    string newFolder = Combine(
+        GetFolderPath(SpecialFolder.Personal), "Code","Chapter09","NewFolder"
+        );
+    WriteLine($"Working with: {newFolder}");
+    //생성폴더 존재 확인
+    WriteLine($"Does it exist ? {Exists(newFolder)}");
+
+    //디렉토리 생성
+    WriteLine("Creating it...");
+    CreateDirectory(newFolder);
+    WriteLine($"Does it exist ? {Exists(newFolder)}");
+    Write("Confirm the directory exists, and then press ENTER: ");
+    ReadLine();
+    //삭제
+    WriteLine("Deleting it...");
+    Delete(newFolder,recursive:true);
+}
+//파일 다루기
+WorkWithFiles();
+static void WorkWithFiles()
+{
+    // 사용자 폴더에 새 디렉토리 경로 지정
+    string dir = Combine(
+            GetFolderPath(SpecialFolder.Personal), "Code", "Chapter09", "OutputFiles"
+        );
+    CreateDirectory(dir);
+
+    //파일경로지정
+    string textFile = Combine(dir, "Dummy.txt");
+    string backupFile = Combine(dir, "Dummy.bak");
+    WriteLine($"Working with : {textFile}");
+    //파일존재 확인
+    WriteLine($"Does it exist? {File.Exists(textFile)}");
+
+    //파일 생성 후, 파일 안에 1줄의 텍스트 입력
+    StreamWriter textWriter = File.CreateText(textFile);
+    textWriter.WriteLine("Hello, C#!");
+    textWriter.Close();
+    WriteLine($"Does it exist? {File.Exists(textFile)}");
+    //파일 복사
+    File.Copy(sourceFileName:textFile,destFileName:backupFile,overwrite:true);
+    WriteLine($"Does {backupFile} exist? {File.Exists(backupFile)}");
+    Write("Confirm the files exist, and then press ENTER: ");
+    ReadLine();
+    //파일 삭제
+    File.Delete(textFile);
+    WriteLine($"Does it exist? {File.Exists(textFile)}");
+    //백업한 파일에서 읽기
+    WriteLine($"Reading contents of {backupFile}");
+    StreamReader textReader = File.OpenText(backupFile);
+    WriteLine(textReader.ReadToEnd());
+    textReader.Close();
+
+
+    //경로
+    WriteLine($"Folder Name : {GetDirectoryName(textFile)}");
+    WriteLine($"File Name : {GetFileName(textFile)}");
+    WriteLine($"File Name without Extension {0}" , GetFileNameWithoutExtension(textFile));
+    WriteLine($"File Extension : {GetExtension(textFile)}");
+    WriteLine($"Random File Name : {GetRandomFileName()}");
+    WriteLine($"Temporary File Name : {GetTempFileName()}");
+
+    //파일의 정보 얻기
+    FileInfo info = new(backupFile);
+    WriteLine($"{backupFile} : ");
+    WriteLine($"Contains {info.Length} bytes");
+    WriteLine($"Last accessed {info.LastAccessTime}");
+    WriteLine($"Has readonly set to {info.IsReadOnly}");
+}
---
+++ WorkingWithStreams/Program.cs
@@ -0,0 +1,157 @@
+﻿using System.Xml;
+using static System.Console;
+using static System.Environment;
+using static System.IO.Path;
+using System.IO.Compression; //BrotliStream,GZipStream, CompressionMode
+
+//Viper.WorkWithText();
+//Viper.WorkWithXml();
+Viper.workWithCompression();
+Viper.workWithCompression(useBrotil : false);
+static class Viper
+{
+    public static string[] Callsigns = new[]
+    {
+        "Husker","Starbuck","Apollo","Boomer",
+        "Bulldog","Athena","Helo","Racetrack"
+    };
+
+    public static void WorkWithText()
+    {
+        string textFile = Combine(CurrentDirectory, "streams.txt");
+
+        //파일 생성
+        StreamWriter text = File.CreateText(textFile);
+
+        foreach (string item in Viper.Callsigns)
+        {
+            text.WriteLine(item);
+        }
+        text.Close();
+        //출력
+        WriteLine("{0} conatins {1:N0} bytes.", arg0: textFile, arg1: new FileInfo(textFile).Length);
+        WriteLine(File.ReadAllText(textFile));
+    }
+
+   public static void WorkWithXml()
+    {
+
+        FileStream? xmlFileStream = null;
+        XmlWriter? xml = null;
+        try
+        {
+            //파일 정의
+            string xmlFile = Combine(CurrentDirectory, "streams.xml");
+
+            //파일스트림 생성
+             xmlFileStream = File.Create(xmlFile);
+
+            //Xml writer 헬퍼 -> 파일 스르팀 래핑 후 중첩요소 들여오기
+             xml = XmlWriter.Create(xmlFileStream, new XmlWriterSettings { Indent = true });
+
+            //파일에 Xml선언
+            xml.WriteStartDocument();
+
+            //root element
+            xml.WriteStartElement("callsigns");
+
+            foreach (string item in Viper.Callsigns)
+            {
+                xml.WriteElementString("callsign", item);
+            }
+
+            xml.WriteEndElement();
+            xml.Close();
+            xmlFileStream.Close();
+
+            WriteLine("{0} contains {1:N0} bytes.", arg0: xmlFile, arg1: new FileInfo(xmlFile).Length);
+            WriteLine(File.ReadAllText(xmlFile));
+        }catch(Exception ex)
+        {
+            //경로 미존재
+            WriteLine($"{ex.GetType()} says {ex.Message}");
+        }
+        finally
+        {
+            if(xml != null)
+            {
+                xml.Dispose();
+                WriteLine("The XML writer`s unmanaged resources have been disposed.");
+            }
+            if(xmlFileStream != null)
+            {
+                xmlFileStream.Close();
+                WriteLine("The file stream`s unmanaged resources have been disposed.");
+            }
+        }
+    }
+
+    public static void workWithCompression(bool useBrotil = true)
+    {
+        string fileExt = useBrotil ? "brotil" : "gzip";
+        string filePath = Combine(CurrentDirectory, $"stream.{fileExt}");
+        FileStream file = File.Create(filePath);
+
+        Stream compressor;
+        if(useBrotil)
+        {
+            compressor = new BrotliStream(file, CompressionMode.Compress);
+        }
+        else
+        {
+            compressor = new GZipStream(file, CompressionMode.Compress);
+        }
+
+        using (compressor)
+        {
+            using(XmlWriter xml = XmlWriter.Create(compressor))
+            {
+                xml.WriteStartDocument();
+                xml.WriteStartElement("callsigns");
+                foreach(string item in Viper.Callsigns)
+                {
+                    xml.WriteStartElement("callsign",item);
+                }
+            }
+        }
+
+        //압축파일 내용출력
+        WriteLine("{0} contains {1:N0} bytes.", filePath, new FileInfo(filePath).Length);
+       // WriteLine($"The compressed contents: ");
+        //WriteLine(File.ReadAllText(filePath));
+
+        WriteLine("Reading the compressed XML file: ");
+        file = File.Open(filePath, FileMode.Open);
+
+        Stream decompressor;
+        if (useBrotil)
+        {
+            decompressor = new BrotliStream(file,CompressionMode.Decompress);
+        }
+        else
+        {
+            decompressor = new GZipStream(file, CompressionMode.Decompress);
+        }
+
+        using (decompressor)
+        {
+            using(XmlReader reader = XmlReader.Create(decompressor))
+            {
+                while (reader.Read())
+                {
+                    //노드이름이 callsign 확인
+                    if((reader.NodeType == XmlNodeType.Element) && (reader.Name == "callsign"))
+                    {
+                        reader.Read();
+                        WriteLine($"{reader.Value}");
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
